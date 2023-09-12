838435aded8b6ee102a1f2d6026a5d71233dc49f
ebenemfum <65869086+ebenemfum@users.noreply.github.com>
2023-09-12 10:21:29 +0200

    Task 2 Completed

---
+++ Phase1/Task2.php
@@ -0,0 +1,18 @@
+
+Task 2: Control Structure and Loops
+1. Control Structures: Study control flow mechanisms like conditionals (if statements, switch statements) and loops (for loops, while loops), which help you manage the flow of your program.
+2. Build a program that iterates through an array and displays its content.
+
+    <?php
+
+//Array
+$numbers = [1,2,3,4,5,6,8];
+echo "Numbers in the Array are : " ;
+foreach($numbers as $number){
+    echo "$number ";
+};
+
+    ?>
+
+
+
