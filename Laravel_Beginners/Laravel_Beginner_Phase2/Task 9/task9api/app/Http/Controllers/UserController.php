d191e979f08d66f148f462a85c7b42ebd08bc528
ebenemfum <65869086+ebenemfum@users.noreply.github.com>
2023-11-12 16:49:44 +0000

    API Created

---
+++ Laravel_Beginners/Laravel_Beginner_Phase2/Task 9/task9api/app/Http/Controllers/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    // Create a new user
+    public function createUser(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return response()->json(['user' => $user, 'message' => 'User created successfully'], 201);
+    }
+
+    // Update an existing user
+    public function updateUser(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'string',
+            'email' => 'email|unique:users,email,' . $id,
+            'password' => 'string|min:6',
+        ]);
+
+        $user = User::findOrFail($id);
+
+        $user->update([
+            'name' => $request->input('name', $user->name),
+            'email' => $request->input('email', $user->email),
+            'password' => $request->has('password') ? Hash::make($request->password) : $user->password,
+        ]);
+
+        return response()->json(['user' => $user, 'message' => 'User updated successfully'], 200);
+    }
+
+    // Delete a user
+    public function deleteUser($id)
+    {
+        $user = User::findOrFail($id);
+        $user->delete();
+
+        return response()->json(['message' => 'User deleted successfully'], 200);
+    }
+    public function showAllUsers()
+    {
+        $users = User::all();
+
+        return response()->json(['users' => $users], 200);
+    }
+
+    
+}
---
+++ Laravel_Beginners/Laravel_Beginner_Phase2/Task 9/task9api/database/seeders/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Clear existing records to start from scratch
+        DB::table('users')->truncate();
+
+        // Seed sample users
+        User::create([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        User::create([
+            'name' => 'Jane Doe',
+            'email' => 'jane@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        // You can add more users as needed
+    }
+}
